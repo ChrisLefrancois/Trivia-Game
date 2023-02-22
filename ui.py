02c4cb86b6a2d8cb2824e33831fabf7d213b765d
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-02-22 13:04:24 -0500

    finished the gui interface

--- ui.py
+++ ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self):
+        self.score = 0
+        self.window = Tk()
+        self.window.title("Quizz")
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+
+        self.score_label = Label()
+        self.score_label.config(text=f"Score: {self.score}", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150, 125,
+            text="Amazon required blah blah",
+            fill="black",
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(column=0, columnspan=2, row=1, pady=50)
+
+        self.right_image = PhotoImage(file="./images/true.png")
+        self.wrong_image = PhotoImage(file="./images/false.png")
+        self.true_button = Button(image=self.right_image, highlightbackground=THEME_COLOR)
+        self.false_button = Button(image=self.wrong_image, highlightbackground=THEME_COLOR)
+        self.true_button.grid(column=0, row=2)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
 
