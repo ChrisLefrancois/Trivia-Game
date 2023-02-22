a691683f672928f55c4ca47eac8c0f053fcf3273
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-02-22 13:11:56 -0500

    added the quiz to the ui

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.score = 0
         self.window = Tk()
         self.window.title("Quizz")
@@ -16,7 +19,9 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
         self.question_text = self.canvas.create_text(
-            150, 125,
+            150,
+            125,
+            width=280,
             text="Amazon required blah blah",
             fill="black",
             font=("Arial", 20, "italic")
@@ -30,5 +35,10 @@ class QuizInterface:
         self.true_button.grid(column=0, row=2)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
 
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
